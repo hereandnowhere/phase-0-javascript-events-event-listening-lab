0a39a53f7f6b7bfd6f323f9c7b95b9fc74a8d192
Jacob Vine <jvine1235@gmail.com>
2022-02-21 13:29:31 -0500

    commit tree 46ec1c1184870f599cd7bc5f8029928cd1e54a4d

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function addingEventListener() {
     const input = document.getElementById('input');
 
     function clickAlert() {
-     alert('I was clicked!');
+     alert('I was clicked! Yay!');
     }
 
     input.addEventListener('click', clickAlert);
